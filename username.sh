9b2a247a2679fb7e50e240a6e481552b6c808534
Dylan McIntosh <dmcintosh@chapman.edu>
2020-11-13 10:56:44 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,9 +3,12 @@
 # Dylan McIntosh
 echo "Enter a Username: "
 read -r NAME
-while echo "$NAME" | grep -E -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid Username!"
+	echo "Between 3 and 12 characters"
+	echo "Start with a lowercase letter"
+	echo "Can only use lower case letters, digits, and underscore"
 	echo "Enter a valid Username: "
 	read -r NAME
 done
