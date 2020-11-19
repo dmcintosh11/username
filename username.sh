b42fa9dccca61b16bd858cbe4d1ee6ce4dfc439d
Dylan McIntosh <dmcintosh@chapman.edu>
2020-11-19 00:22:11 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 # username.sh
 # Dylan McIntosh
+
+echo "Between 3 and 12 characters"
+echo "Start with a lowercase letter"
+echo "Can only use lower case letters, digits, and underscore"
 echo "Enter a Username: "
 read -r NAME
 while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
